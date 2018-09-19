3406081e56a1b2895f50c88ef51c6926ab6b7b8c
Mark Goldwater <mark.goldwater@students.olin.edu>
2018-09-19 19:46:01 -0400

    changed rospy.debuglog to rospy.loginfo

--- iarc_forebrain/scripts/star.py
+++ iarc_forebrain/scripts/star.py
@@ -5,32 +5,32 @@ from util.Drone import Drone
 
 drone = Drone()
 
-rospy.logdebug("Drone about to take off...")
+rospy.loginfo("Drone about to take off...")
 drone.takeoff()
 
-rospy.logdebug("Moving to origin 1...")
+rospy.loginfo("Moving to point 1...")
 drone.move_to(0.0, 0.0)
 drone.hover(1)
 
-rospy.logdebug("Moving to point 2...")
+rospy.loginfo("Moving to point 2...")
 drone.move_to(-3.5, -3.0)
 drone.hover(1)
 
-rospy.logdebug("Moving to point 3...")
+rospy.loginfo("Moving to point 3...")
 drone.move_to(-2.0, 2.0)
 drone.hover(1)
 
-rospy.logdebug("Moving to point 4...")
+rospy.loginfo("Moving to point 4...")
 drone.move_to(-0.5, -3.0)
 drone.hover(1)
 
-rospy.logdebug("Moving to point 5...")
+rospy.loginfo("Moving to point 5...")
 drone.move_to(-4.0, 0.0)
 drone.hover(1)
 
-rospy.logdebug("Moving back to origin...")
+rospy.loginfo("Moving back to point 1...")
 drone.move_to(0.0, 0.0)
 drone.hover(3)
 
-rospy.logdebug("Landing...")
+rospy.loginfo("Landing...")
 drone.land()
