371558591427f3447e6409c9998f618a3c0ecc04
nathanestill <nathan.estill@students.olin.edu>
2018-10-17 20:32:04 -0400

    changing more

--- iarc_fuses/scripts/QRDetector.py
+++ iarc_fuses/scripts/QRDetector.py
@@ -7,9 +7,10 @@ import rospkg
 
 rospack = rospkg.RosPack() 
 pkgRoot = rospack.get_path('iarc_fuses') # Gets the package
-file = os.path.join(pkgRoot,"QRCutPics","0145_4_TableFlat.jpg")
+file = os.path.join(pkgRoot,"QRCutPics","0145_2_TableFlat.jpg")
 image = cv2.imread(file)
-imageR = cv2.resize(image, (960, 540)) 
+imageR = cv2.resize(image, (960, 540))
+#imageR = cv2.GaussianBlur(imageR,(1,1),0) 
 # cv2.imshow("Image",image)
 # cv2.waitKey(0)
 blackBound1 = (0,0,0)
@@ -46,24 +47,26 @@ for i in maxcnt:
     # if self._aspect_bound[0] < aspect < self._aspect_bound[1]:
     boxes.append(box)
     cv2.drawContours(imageR, [box], 0, (0,0,255), 2)
-left = [-1,-1]
-right = [-1,-1]
-top = [-1,-1]
-bot = [-1,-1]
-for boxxx in boxes:
-	for vert in boxxx:
-		if vert[0] < left[0] or left[0] == -1:
-			left = vert
-		if vert[0] > right[0] or right[0] == -1:
-			right = vert
-		if vert[1] < bot[1] or bot[1] == -1:
-			bot = vert
-		if vert[1] > top[1] or top[1] == -1:
-			top = vert
-bigBox = [left,top,right,bot]
-print(bigBox)
-cv2.imshow("Box",imageR)
-cv2.waitKey(0)
+
+# left = [-1,-1]
+# right = [-1,-1]
+# top = [-1,-1]
+# bot = [-1,-1]
+# for boxxx in boxes:
+# 	for vert in boxxx:
+# 		if vert[0] < left[0] or left[0] == -1:
+# 			left = vert
+# 		if vert[0] > right[0] or right[0] == -1:
+# 			right = vert
+# 		if vert[1] < bot[1] or bot[1] == -1:
+# 			bot = vert
+# 		if vert[1] > top[1] or top[1] == -1:
+# 			top = vert
+# bigBox = [left,top,right,bot]
+# nbigBox = np.asarray(bigBox)
+#cv2.drawContours(imageR,[nbigBox],0,(0,255,0),2)
+# cv2.imshow("Box",imageR)
+# cv2.waitKey(0)
 
 
 # I have to convert this to a box
