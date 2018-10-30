3969649559cffaaa0c4b41c2466d5a30528413cb
Jamie Cho <jchocholate@gmail.com>
2018-10-30 15:55:19 -0400

    add demo script + fix transforms

--- iarc_fuses/scripts/demo.sh
+++ iarc_fuses/scripts/demo.sh
@@ -2,8 +2,8 @@
 
 tmux \
   new-session  "roslaunch iarc_sim_3d sim.launch single:=false map_tf:=true" \; \
-  split-window "sleep 5; rostopic pub /drone_1/ardrone/takeoff std_msgs/Empty "{}" --once" \; \
-  split-window "sleep 5; rostopic pub /drone_2/ardrone/takeoff std_msgs/Empty "{}" --once" \; \
+  split-window "roslaunch iarc_fuses hud.launch" \; \
   split-window "ROS_NAMESPACE=/drone_1 rosrun teleop_twist_keyboard teleop_twist_keyboard.py" \; \
-  split-window "ROS_NAMESPACE=/drone_2 rosrun teleop_twist_keyboard teleop_twist_keyboard.py" \; \
-  select-layout even-vertical
+  split-window -h "ROS_NAMESPACE=/drone_2 rosrun teleop_twist_keyboard teleop_twist_keyboard.py" \; \
+  split-window "rostopic pub /drone_1/ardrone/takeoff std_msgs/Empty "{}" --latch" \; \
+  split-window "rostopic pub /drone_2/ardrone/takeoff std_msgs/Empty "{}" --latch" \;
