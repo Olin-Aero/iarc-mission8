d04a6776b9ce945842a5bf9ee3ea866baf45caec
David Tarazi <dtarazi@olin.edu>
2019-03-01 16:49:28 -0500

    Dieter and I worked through getting what we think will work to control multiple drones, but probably need a code review and definitely need to test

--- iarc_main/scripts/bebop_keyboard_multi.py
+++ iarc_main/scripts/bebop_keyboard_multi.py
@@ -1,4 +1,53 @@
+import roslib
+
+roslib.load_manifest('teleop_twist_keyboard')
+import rospy
+
+from geometry_msgs.msg import Twist, Vector3
+from std_msgs.msg import Empty, String
+
+import sys, select, termios, tty
+
+""" DONT RUN THIS CODE W/O DIETER OR DAVID """
+
 class Drone:
+    """ Class for representing a bebop drone """
+
+    msgTeleop = """
+    You are now running teleop!
+
+    Reading from the keyboard and Publishing to Twist!
+    ---------------------------
+    Moving around (strafing):
+       u    i    o
+       j    k    l
+       m    ,    .
+    --------------------------
+    Up, Down, Turning
+
+        w
+      a   d
+        s
+
+    For Turning mode, hold down the shift key:
+    ---------------------------
+       U    I    O
+       J    K    L
+       M    <    >
+
+    takeoff: t
+    land: spacebar
+    Cut all motors: '=' or '+'
+    Switch to autonomous mode: 'h' or 'H'
+
+    anything else : stop
+
+    q/z : increase/decrease max speeds by 10%
+    r/v : increase/decrease only linear speed by 10%
+    e/c : increase/decrease only angular speed by 10%
+
+    CTRL-C to quit
+    """
 
     takeoffLand = {
         't': (1, 0),
@@ -38,6 +87,11 @@ class Drone:
     }
 
     def __init__(self, namespace_in, namespace_out, bind):
+        """ initializes the drone class
+        namespace_in:
+        namespace_out: namespace to send to bebop driver. no slashes.
+        bind: char that represents control key for this drone instance
+        """
         self.flight_status = False #initializes in teleop
         self.namespace_in = str(namespace_in) #namespace for subs
         self.namespace_out = str(namespace_out) #namespace for pubs
@@ -48,16 +102,17 @@ class Drone:
         self.z = 0
         self.th = 0
         self.status = 0
-        self.speed =
+        self.speed = rospy.get_param("~speed", 0.1)
+        self.turn = rospy.get_param("~turn", 0.2)
 
         self.cmd_pub = rospy.Publisher('/' + self.namespace_out + '/cmd_vel', Twist, queue_size=1)
         self.takeoff_pub = rospy.Publisher('/' + self.namespace_out + '/takeoff', Empty, queue_size=1)
         self.land_pub = rospy.Publisher('/' + self.namespace_out + '/land', Empty, queue_size=1)
         self.reset_pub = rospy.Publisher('/' + self.namespace_out + '/reset', Empty, queue_size=1)
 
+        self.cmd_sub = rospy.Subscriber('/' + self.namespace_in + '/cmd_vel', Twist, callback_cmd_vel)
         self.takeoff_sub = rospy.Subscriber('/' + self.namespace_in +'/takeoff', Empty, callback_takeoff)
         self.land_sub = rospy.Subscriber('/' + self.namespace_in + '/land', Empty, callback_land)
-        self.cmd_sub = rospy.Subscriber('/' + self.namespace_in + '/cmd_vel', Twist, callback_cmd_vel)
 
 
     def callback_cmd_vel(self, msg):
@@ -78,8 +133,120 @@ class Drone:
         else:
             rospy.loginfo_throttle(1, "land command ignored\r")
 
-
     def get_vels(self):
+        """
+        returns the current linear and angular velocities
+        """
         return "currently:\tspeed %s\tturn %s " % (self.speed, self.turn)
 
+    def finish(self):
+        """
+        Does the commands to hover, then land the drone upon pressing CTRL-C
+        """
+        self.cmd_pub.publish(Twist())
+        self.land_pub.publish()
+
     def handle_key(self, key):
+        """
+        Takes in the key and handles what to do with it
+        If nothing is pressed, do nothing
+        """
+        if key in moveBindings.keys():
+            self.x = moveBindings[key][0]
+            self.y = moveBindings[key][1]
+            self.z = moveBindings[key][2]
+            self.th = moveBindings[key][3]
+        elif key in speedBindings.keys():
+            self.speed = self.speed * speedBindings[key][0]
+            self.turn = self.turn * speedBindings[key][1]
+
+            print get_vels(self.speed, self.turn)
+            if self.status == 14:
+                print msgTeleop
+            self.status = (self.status + 1) % 15
+        else:
+            self.x = 0
+            self.y = 0
+            self.z = 0
+            self.th = 0
+
+        cmd_pub.publish(Twist(
+                linear=Vector3(x=self.x * self.speed, y=self.y * self.speed, z=self.z * self.speed),
+                angular=Vector3(z=self.th * self.turn)
+                ))
+
+def getKey():
+    tty.setraw(sys.stdin.fileno())
+    select.select([sys.stdin], [], [], 0)
+    key = sys.stdin.read(1)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+if __name__ == "__main__":
+    settings = termios.tcgetattr(sys.stdin)
+    print msgTeleop
+
+    # init all drones used
+    drone1 = Drone('cmd_bebop1', 'bebop1', '1')
+    drone2 = Drone('cmd_bebop2', 'bebop2', '2')
+    drone3 = Drone('cmd_bebop3', 'bebop3', '3')
+    drone4 = Drone('cmd_bebop4', 'bebop4', '4')
+    all_drones = [drone1, drone2, drone3, drone4]
+
+    # start with all drones active
+    active_drones = all_drones
+
+    try:
+        while not rospy.is_shutdown():
+            key = getKey()
+
+            # CTRL-C quits and lands drone
+            if key == '\x03':
+                break
+
+            # change drone from teleop to autonomous
+            if key == 'h' or key == "H":
+
+                for d in active_drones:
+                    d.flight_status = True
+
+                rospy.loginfo("In autonomous mode!")
+            else:
+                # a teleop key is pressed, kick all all_drones
+                # out of autonomous
+                for d in all_drones:
+                    d.flight_status = False
+
+                # handles reset
+                if key == '=' or key == '+':
+                    for d in active_drones:
+                        d.reset_pub.publish()
+
+                # handle landing and taking off
+                if key in takeoffLand.keys():
+                    if Drone.takeoffLand[key][0] == 1:
+                        for d in active_drones:
+                            d.takeoff_pub.publish()
+                    elif Drone.takeoffLand[key][1] == 1:
+                        for d in active_drones:
+                            d.land_pub.publish()
+
+                # handles keys for all active drones
+                if key = '0':
+                    active_drones = all_drones
+                else:
+                    for d in active_drones:
+                        if key = d.bind:
+                            active_drones = [drone for drone in all_drones if drone.bind == d.bind]
+                    for d in active_drones:
+                        d.handle_key(key)
+
+    except Exception as e:
+        print e
+
+    # hovers and then lands drone if run loop is quit
+    finally:
+        for d in active_drones:
+            d.finish()
+
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
