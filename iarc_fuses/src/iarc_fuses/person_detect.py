544b482c76e288adad0958f1f91011d83eee3dee
Maximilian Schommer <maxschommer@gmail.com>
2019-03-05 22:09:25 -0500

    Made a tracker class

--- iarc_fuses/src/iarc_fuses/person_detect.py
+++ iarc_fuses/src/iarc_fuses/person_detect.py
@@ -6,57 +6,207 @@ from __future__ import print_function
 from imutils.object_detection import non_max_suppression
 import numpy as np
 import imutils
+from object_track import Object_Tracker
 import cv2
+import utils
 
-# initialize the HOG descriptor/pels
-# rson detector
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+def main2():
+    vidPath = '/home/maximilian/Pictures/VID_20190220_192021.mp4'
+    tracker = Object_Tracker()
+    cap = cv2.VideoCapture(vidPath)
 
-vidPath = '/home/maximilian/Pictures/VID_20190220_192021.mp4'
-cap = cv2.VideoCapture(vidPath)
-
-while(cap.isOpened()):
+    # Start tracker by initializing state
     ret, image = cap.read()
+    tracker.start_tracking(image, [334, 128, 438, 188])
 
-    image = imutils.resize(image, width=min(400, image.shape[1]))
-    orig = image.copy()
+    startedTracking = False
+    while cap.isOpened():
+        ret, image = cap.read()
+        print(type(image))
 
-    # detect people in the image
-    (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
-                                            padding=(8, 8), scale=1.05)
+        res = utils.convert_to_pixels(np.shape(image), tracker.track(image))
+        print(tracker.state)
+        cv2.rectangle(image, (res[0], res[1]), (res[0] + res[2], res[1] + res[3]), (0, 255, 255), 3)
+        # Show image
+        cv2.imshow("object detection", image)
 
-    # draw the original bounding boxes
-    for (x, y, w, h) in rects:
-        cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        # Press q to quit
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    # apply non-maxima suppression to the bounding boxes using a
-    # fairly large overlap threshold to try to maintain overlapping
-    # boxes that are still people
-    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+def main():
+    # initialize the HOG descriptor/pels
+    # rson detector
 
-    # draw the final bounding boxes
-    for (xA, yA, xB, yB) in pick:
-        cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+    vidPath = '/home/maximilian/Pictures/VID_20190220_192021.mp4'
+    classPath = 'yolov3.txt'
+    weights = 'yolov3.weights'
+    config = 'yolov3.cfg'
+    tracker = Object_Tracker()
+    cap = cv2.VideoCapture(vidPath)
 
-    # show some information on the number of bounding boxes
-    filename = vidPath[vidPath.rfind("/") + 1:]
-    print("[INFO] {}: {} original boxes, {} after suppression".format(
-        filename, len(rects), len(pick)))
+    startedTracking = False
+    while cap.isOpened():
+        ret, image = cap.read()
+        image = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+        Width = image.shape[1]
+        Height = image.shape[0]
+        scale = 0.00392
 
-    # show the output images
-    cv2.imshow("Before NMS", orig)
-    cv2.imshow("After NMS", image)
-    cv2.waitKey(0)
+        # read class names from text file
+        classes = None
+        with open(classPath, 'r') as f:
+            classes = [line.strip() for line in f.readlines()]
 
-    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #
-    # cv2.imshow('frame',gray)
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     break
+        # generate different colors for different classes
+        COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
 
-cap.release()
-cv2.destroyAllWindows()
-#
-# for imagePath in imagePaths:
+        # read pre-trained model and config file
+        net = cv2.dnn.readNet(weights, config)
+
+        # create input blob
+        blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
+
+        # set input blob for the network
+        net.setInput(blob)
+
+        if not startedTracking:
+            # run inference through the network
+            # and gather predictions from output layers
+            outs = net.forward(get_output_layers(net))
+
+            # initialization
+            class_ids = []
+            confidences = []
+            boxes = []
+            conf_threshold = 0.5
+            nms_threshold = 0.4
+
+            # for each detetion from each output layer
+            # get the confidence, class id, bounding box params
+            # and ignore weak detections (confidence < 0.5)
+            for out in outs:
+                for detection in out:
+                    scores = detection[5:]
+                    class_id = np.argmax(scores)
+                    confidence = scores[class_id]
+                    if confidence > 0.5:
+                        center_x = int(detection[0] * Width)
+                        center_y = int(detection[1] * Height)
+                        w = int(detection[2] * Width)
+                        h = int(detection[3] * Height)
+                        x = center_x - w / 2
+                        y = center_y - h / 2
+                        class_ids.append(class_id)
+                        confidences.append(float(confidence))
+                        boxes.append([x, y, w, h])
+
+
+            # apply non-max suppression
+            indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+            # go through the detections remaining
+            # after nms and draw bounding box
+            for i in indices:
+                if (classes[int(i)] == 'person'):
+                    i = i[0]
+                    box = boxes[i]
+                    x = box[0]
+                    y = box[1]
+                    w = box[2]
+                    h = box[3]
+                    bbox = [x, y, w, h]
+                    tracker.start_tracking(image, bbox)
+                    draw_bounding_box(image, class_ids[i], confidences[i], round(x), round(y), round(x + w), round(y + h), classes, COLORS)
+            print("Done")
+
+
+        # display output image
+        cv2.imshow("object detection", image)
+
+        # # wait until any key is pressed
+        # cv2.waitKey()
+
+        # save output image to disk
+        cv2.imwrite("object-detection.jpg", image)
+
+        # # release resources
+        # cv2.destroyAllWindows()
+
+        ########### HOG STARTS HERE ##############
+        # hog = cv2.HOGDescriptor()
+        # hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+        #
+        # image = imutils.resize(image, width=min(400, image.shape[1]))
+        # orig = image.copy()
+        #
+        # # detect people in the image
+        # (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
+        #                                         padding=(8, 8), scale=.5)
+        #
+        # # draw the original bounding boxes
+        # for (x, y, w, h) in rects:
+        #     cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        #
+        # # apply non-maxima suppression to the bounding boxes using a
+        # # fairly large overlap threshold to try to maintain overlapping
+        # # boxes that are still people
+        # rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+        # rects = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+        #
+        # # draw the final bounding boxes
+        # for (xA, yA, xB, yB) in rects:
+        #     cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+        #
+        # # show some information on the number of bounding boxes
+        # filename = vidPath[vidPath.rfind("/") + 1:]
+        # print("[INFO] {}: {} original boxes, {} after suppression".format(
+        #     filename, len(rects), len(rects)))
+        #
+        # # show the output images
+        # cv2.imshow("Before NMS", orig)
+        # cv2.imshow("After NMS", image)
+        # # cv2.waitKey(0)
+        #
+        # # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # #
+        # # cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        ################## HOG ENDS HERE ########################
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+# function to get the output layer names
+# in the architecture
+def get_output_layers(net):
+    layer_names = net.getLayerNames()
+
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    return output_layers
+
+
+# function to draw bounding box on the detected object with class name
+def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h, classes, COLORS):
+    label = str(classes[class_id])
+
+    color = COLORS[class_id]
+    # print(img, (x, y), (x_plus_w, y_plus_h), int(color[0]))
+    # cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), int(color[0]), 2)
+
+    pt1 = (int(x), int(y))
+    pt2 = (int(x_plus_w), int(y_plus_h))
+
+    cv2.rectangle(img, pt1, pt2, (0, 255, 0), 3)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    # cv2.putText(img, 'OpenCV', (10, 500), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+    # cv2.rectangle(img, pt1, pt2, int(color[0]))
+    cv2.putText(img, label, (int(x - 10), int(y - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
+if __name__ == '__main__':
+    # main()
+    main2()
--- iarc_fuses/src/iarc_fuses/utils.py
+++ iarc_fuses/src/iarc_fuses/utils.py
@@ -1,8 +1,68 @@
 import numpy as np
+
+
 def rint(x):
+    """Rounds a float to a 32 bit integer
+    
+    Args:
+        x (float): Input value
+    
+    Returns:
+        np.int32: Rounded int32
+    """
     return np.int32(np.round(x))
 def anorm(x):
+    """Normalizes an angle to exist between -pi and pi
+    
+    Args:
+        x (float): Angle in radians
+    
+    Returns:
+        float: Angularly normalized angle
+    """
     return (x+np.pi) % (2*np.pi) - np.pi
+
 def adiff(a,b):
+    """Differnece between two angles, a and b represented in radians
+    
+    Args:
+        a (float): Angle a in radians
+        b (float): Angle b in radians
+    
+    Returns:
+        float: Normalized difference
+    """
     return anorm(a-b)
 
+def convert_to_relative(image_size, bounding_box):
+    """Converts an absolute bounding box into a relative one given an image image_size
+    
+    Args:
+        image_size (2-tuple): Size of form (width, height)
+        bounding_box (4-tuple): Pixel defined bounding box of object, in 
+                form (center_x, center_y, width, height)
+
+    Returns:
+        4-tuple(float): Returns a relative bounding box of type (center_x, center_y, width, height) 
+    """
+    print(image_size)
+    print(bounding_box)
+    return (bounding_box[0]/image_size[0], bounding_box[1]/image_size[1], 
+            bounding_box[2]/image_size[0], bounding_box[3]/image_size[1])
+
+def convert_to_pixels(image_size, bounding_box):
+	"""Converts a relative bounding box into a bounding box defined using pixels
+	given an image size
+	
+	Args:
+	    image_size (2-tuple): Size of form (width, height)
+        bounding_box (4-tuple): Relative defined bounding box of object, in 
+                form (center_x, center_y, width, height)
+	
+	Returns:
+	    4-tuple(float): Returns a pixel defined bounding box of type (center_x, center_y, width, height)
+	"""
+	return (int(bounding_box[0] * image_size[0]),
+			int(bounding_box[1] * image_size[1]),
+			int(bounding_box[2] * image_size[0]),
+			int(bounding_box[3] * image_size[1]))
