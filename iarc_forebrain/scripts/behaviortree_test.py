487dc7107f3aa88eec2b425cfa204d4108998274
Eric Miller <eric@legoaces.org>
2019-04-03 19:04:28 -0400

    Behavior tree work

--- iarc_forebrain/scripts/behaviortree_test.py
+++ iarc_forebrain/scripts/behaviortree_test.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python2
 import rospy
 
-from iarc_forebrain.behaviortree import Behavior
+from iarc_forebrain.behaviortree import Behavior, Result
+
+class Sequence(Behavior):
+    def __init__(self, name=None, children=[]):
+        super(Sequence, self).__init__(name, children)
+        self.index = None
+
+    def reset(self):
+        if self.index is not None:
+            self.children[self.index].reset()
+        self.index=0
+
+    def run()
+
+class SayHi(Behavior):
+    def reset(self):
+        self.message = "hi"
+
+    def run(self):
+        print(self.message)
+        return Result.SUCCESS
 
 tree = Behavior('hello', [
     Behavior('world', [
         Behavior(),
-        Behavior(),
+        SayHi(),
     ])])
 
 print(tree)
+
+tree.reset()
+tree.run()
