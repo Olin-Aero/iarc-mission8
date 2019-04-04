3ae3094478f3bd99a2cc4ce8a83b528aad58f63a
--global <nathan.estill@students.olin.edu>
2019-04-03 20:07:30 -0400

    deleting the test

--- iarc_forebrain/scripts/gimbalTest.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2
-import rospy
-from util.Drone import Drone
-
-cam = Drone()
-cam.go_to_camera(-45.0,0.0,0.0)
-cam.go_to_camera(-30.0,-15.0,0.0)
-cam.go_to_camera(-30.0,0.0,-15.0)
