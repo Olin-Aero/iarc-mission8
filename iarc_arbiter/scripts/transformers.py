a606909fb7d145d3d98ceecea97ddd0047d16273
Paul <superduperpacman42@gmail.com>
2019-04-10 20:24:50 -0400

    Multidrone works in sim

--- iarc_arbiter/scripts/transformers.py
+++ iarc_arbiter/scripts/transformers.py
@@ -63,6 +63,10 @@ class PIDAltController(object):
         self.config.add_variable("min_vertical_vel", "Minimum speed the drone is allowed to ascend or descend",
                                  rospy.get_param('~min_vertical_vel', 0.1), 0.0, 3.0)
 
+
+        self.base_link_frame = rospy.get_param('tf_prefix', '')+'/base_link'
+        self.odom_frame = rospy.get_param('tf_prefix', '')+'/odom'
+
     def calculate_z_vel(self, error):
         if abs(error) <= 0.05:
             # Good enough! Stop publishing height changing
@@ -79,7 +83,7 @@ class PIDAltController(object):
         return vel
 
     def get_height(self):
-        return self.tf.lookupTransform('odom', 'base_link', rospy.Time(0))[0][2]
+        return self.tf.lookupTransform(self.odom_frame, self.base_link_frame, rospy.Time(0))[0][2]
 
     def cmd_vel_alt(self, msg):
         """
@@ -138,9 +142,6 @@ class PIDPosController(object):
         self.integral_y = 0.0
         self.previous_error_y = 0.0
 
-        self.base_link_frame = rospy.get_param('tf_prefix', '')+'/base_link'
-        self.odom_frame = rospy.get_param('tf_prefix', '')+'/odom'
-
     def on_odom(self, msg):
         """
         :param Odometry msg:
@@ -157,7 +158,7 @@ class PIDPosController(object):
 
         vel = Twist()
 
-        pose = self.transformPoseFull(self.base_link_frame, msg, self.odom_frame)
+        pose = self.transformPoseFull(self.alt_controller.base_link_frame, msg, self.alt_controller.odom_frame)
         position = pose.pose.position
         # position = self.tf.transformPose('base_link', msg).pose.position
 
@@ -284,9 +285,6 @@ class PIDPosCamController(object):
 
         self.last_time = rospy.Time(0)
 
-        self.base_link_frame = rospy.get_param('tf_prefix', '')+'/base_link'
-        self.odom_frame = rospy.get_param('tf_prefix', '')+'/odom'
-
     def cmd_pos_cam(self, msg):
         """
         inputs a pose for the drone to go to, and a position for the drone to face.
@@ -296,7 +294,9 @@ class PIDPosCamController(object):
         Not transforming the position here would result in the position still being in the map frame
         and not the drone frame.
         """
-        focus = self.pos_controller.transformPoseFull(self.base_link_frame,msg.look_at_position,self.odom_frame)
+        focus = self.pos_controller.transformPoseFull(self.pos_controller.alt_controller.base_link_frame,
+            msg.look_at_position,
+            self.pos_controller.alt_controller.odom_frame)
         orientation_to_look_at = math.atan2(focus.pose.position.y,focus.pose.position.x)
         pose_sum = msg.pose_stamped
         return self.pos_controller.cmd_pos(pose_sum, orientation_to_look_at)
