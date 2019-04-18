b49d70556bd6e34179b75be1cb27622a7e3e8824
Adam Selker <adamselker@gmail.com>
2019-04-17 20:45:35 -0400

    find person/helmet intersections

--- iarc_fuses/src/iarc_fuses/person_helmet_detect.py
+++ iarc_fuses/src/iarc_fuses/person_helmet_detect.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 
 import cv2
+import numpy as np
 import person_detect
 import object_detection_tf 
 
@@ -14,16 +15,27 @@ while True:
 
   objs = person_detector(img) # Find all objs 
 
-  # Convert dict of lists to list of dicts
-  objs = [{'box': b, 'score': s, 'class': c} for (b, s, c) in zip(objs['box'], objs['score'], objs['class'])]
-  objs = [obj for obj in objs if obj['score'] > 0.5 and obj['class'] == 'person'] # limit to high scores
-  for obj in objs:
-    object_detection_tf.draw_bbox(img, obj['box'], str(obj['class']))
+  # Select objects with high scores and the right class, split out their scores and boxes
+  mask = (objs['score'] > 0.5) & (objs['class'] == 'person')
+  scores = objs['score'][mask]
+  boxes = objs['box'][mask] # upper-left y, x, lower-right y, x, as fractions of the image
+  
+  # Draw bounding boxes
+  for (box, score) in zip(boxes, scores):
+    object_detection_tf.draw_bbox(img, box, str(score))
 
+  # Show the thing
   cv2.imshow('win', img)
   k = cv2.waitKey(1)
   if k in [ord('q'), 27]:
       print('quitting...')
       break
 
-  
+  # Search for helmets in the bounding boxes
+  for box in boxes:
+    # Convert fractions to pixel counts
+    bounds = np.round(box * np.array([img.shape[0], img.shape[1], img.shape[0], img.shape[1]])).astype(int)
+    subimg = img[bounds[0]:bounds[2], bounds[1]:bounds[3], :]
+    helmets = person_detect.detect_helmet(subimg)
+    print(helmets)
+
