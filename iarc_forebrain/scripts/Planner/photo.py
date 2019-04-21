1b97ddcc118de4578d938d14a0f514680e6fedb7
--global <nathan.estill@students.olin.edu>
2019-04-21 19:04:41 -0400

    adding photo.py, ImageBin message, and new QR Node

---
+++ iarc_forebrain/scripts/Planner/photo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+import rospy
+from mode import Mode
+from sensor_msgs.msg import Image
+from iarc_forebrain.msg import ImageBin
+from iarc_arbiter.drone import Drone
+
+
+class Photo(Mode):
+	def __init__(self, drone):
+		self.drone = drone
+		rospy.Subscriber(drone.namespace+"image_raw", Image, self.image_raw_callback)
+		self.qrPub = rospy.Publisher("/qr_image", ImageBin, queue_size=10)
+		self.image = None
+	def image_raw_callback(self,data):
+		self.image = data
+
+	def enable(self, binNumber=0):
+		self.active=True
+		imageBinMsg = ImageBin()
+		imageBinMsg.image = self.image
+		imageBinMsg.bin = binNumber
+		if(self.image == None):
+			rospy.logwarn("No images are being published.")
+			return
+		self.qrPub.publish(imageBinMsg)
---
+++ iarc_fuses/scripts/QRNodeOnePipe.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python2
+import rospy
+import sys
+import os
+import cv2
+import numpy as np
+import rospkg
+from sensor_msgs.msg import Image
+from PIL import Image as PILImage
+from std_msgs.msg import Int32
+from math import *
+from cv_bridge import CvBridge, CvBridgeError
+from std_msgs.msg import String
+from iarc_forebrain.msg import ImageBin
+from iarc_fuses.qr_detector import QRDetector
+from iarc_fuses.qr_combiner import QRCombiner
+
+def convert_nparray_to_PIL(x):
+    cv2_im = x
+    cv2_im = cv2.cvtColor(cv2_im,cv2.COLOR_BGR2RGB)
+    return PILImage.fromarray(cv2_im)
+
+class QRNode():
+    def __init__(self):
+        # processing handles
+        self.detector = QRDetector()
+        self.combiner = QRCombiner()
+
+        # ROS Handles
+        rospy.init_node('QRDetector', anonymous=True)
+        self.imageSub = rospy.Subscriber('/qr_image',ImageBin,self.imageCB)
+        self.qrNumberPub = rospy.Publisher('/qr_number', Int32, queue_size=10)
+        self.bridge = CvBridge()
+        self.currentImage = None
+        self.images = [None for _ in range(4)]
+        self.number = None
+        self.nextBin = 0
+
+    def imageCB(self,data):
+        if(data.bin == 0):
+            for image in self.images:
+                if(image == None):
+                    image = self.bridge.imgmsg_to_cv2(data.image, desired_encoding="passthrough")
+            rospy.logwarn("Already have 4 images, call reset")
+        else:
+            self.images[data.bin] = self.bridge.imgmsg_to_cv2(data.image, desired_encoding="passthrough")
+
+    def resetCB(self):
+        # TODO : have a ros topic, service, something that's going to call this
+        self.images = [None for _ in range(4)]
+        self.number=None
+
+    def process(self):
+        # saves the images so that QRCombiner can access them.
+        images = self.images
+        finalImages = []
+        
+        # check if all images are available
+        for image in images:
+            if (image is None):
+                return
+
+        if self.number is None:
+            # don't have the number yet
+            for image in images:
+                croppedImage = self.detector(image)
+                finalImages.append(croppedImage) # adds the image to the collection of final images
+
+            # combine and decode QR Codes
+            pil_images = [convert_nparray_to_PIL(e) for e in finalImages]
+            number = self.combiner(pil_images)
+
+        # now broadcast the result
+        number = self.number
+        print(number)
+        self.qrNumberPub.publish(number)
+
+    def run(self):
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            self.process()
+            rate.sleep()
+
+def main():
+    det = QRNode()
+    det.run()
+
+if __name__ == '__main__':
+    main()
