6148ef5f7f76530a2b8e465897374d95082b828f
Adam Selker <adamselker@gmail.com>
2019-05-05 19:05:11 -0400

    documentation and external usability

--- iarc_fuses/src/iarc_fuses/person_helmet_detect.py
+++ iarc_fuses/src/iarc_fuses/person_helmet_detect.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python2
 
+# TODO: Can we use a neural net to find helmets directly?  This model does
+# not recognize helmets (see yolov3.txt, which afaict lists the things this
+# model can see, though it actually comes from elsewhere.)
+
+# TODO: Dynamically readjust helmet thresholds based on ambient lighting /
+# whole-image properties
+
+# TODO: ROS integration
+
 import cv2
 import numpy as np
 import utils
 import rospy
+
 import helmet_detect
 import object_detection_tf
 import bbox_time_filter
@@ -22,18 +32,33 @@ class Person_Helmet_Tracker:
             min_age=1.0, max_age=0.2, max_side_dist=0.05, max_far_sides=2
         )
 
-    def __call__(self, img, now, show):
+    def __call__(self, img, now=None, show=False):
         """
         Find people and their helmets.
 
         Takes an image and the ros-time that it was taken.  The time is used
         for filtering over time, to prevent jitter.  Detects humans using
         object_detection_tf, and finds helmets within them using helmet_detect.
+        Returns filtered boxes which contain helmets, and the helmets inside
+        them.
+
+        Parameters
+        ----------
+        img : numpy.ndarray
+          The image in which to look for people and helmets.  For best results,
+          keep dimensions constant among calls.
+        now : rospy.rostime.Time
+          The time when the image was taken.  Should monotonically increase;
+          for best results, should strictly increase.
+        show : bool
+          if True, this will draw an opencv window showing bounding boxes and
+          helmet contours.
+
         """
         if now is None:
             now = rospy.get_rostime()
 
-        # Make a copy so we don't mess with the original
+        # Make a copy so we don't mess with the original, which is passed by ref
         if show:
             img_to_show = img.copy()
 
@@ -56,6 +81,7 @@ class Person_Helmet_Tracker:
         # Search for helmets in the bounding boxes
         size_ratio_threshold = 0.01
         size_threshold = 100
+        boxes_with_helmets = []
         helmets = []
         for box in boxes:
             # Convert fractions to pixel counts
@@ -75,6 +101,7 @@ class Person_Helmet_Tracker:
                 helmet_area / box_area > size_ratio_threshold
                 and helmet_area > size_threshold
             ):
+                boxes_with_helmets += [box]
                 helmets += [helmet]
                 if show:
                     cv2.drawContours(img_to_show, [helmet], 0, (255, 255, 255), 1)
@@ -82,11 +109,11 @@ class Person_Helmet_Tracker:
                 if show:
                     cv2.drawContours(img_to_show, [helmet], 0, (255, 0, 0), 1)
 
+        # Show the image with annotations
         if show:
-            cv2.imshow("boxes", img_to_show)
+            cv2.imshow("person_helmet_detect", img_to_show)
 
-
-if __name__ == "__main__":
+        return (boxes_with_helmets, helmets)
 
     rospy.init_node("person_helmet_detect")
 
