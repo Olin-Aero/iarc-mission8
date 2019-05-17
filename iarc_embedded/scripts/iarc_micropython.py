d2e70e76fc9df90c9f11564e9b16319c42145914
Eric Miller <eric@legoaces.org>
2019-05-17 16:46:34 -0400

    Updated embedded code

--- iarc_embedded/scripts/iarc_micropython.py
+++ iarc_embedded/scripts/iarc_micropython.py
@@ -2,6 +2,7 @@ import urequests
 import network
 import utime
 from machine import Pin, UART
+import machine
 
 from config import DRONE_ID
 
@@ -11,21 +12,46 @@ sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
 sta_if.connect("OLIN-ROBOTICS", "R0B0TS-RULE")
 
+
 # response = urequests.post("http://jsonplaceholder.typicode.com/posts", data = "some dummy content")
 
-uart = UART(1)
-enables = [Pin(i, Pin.OUT, None) for i in [16,5,4]]
+# Pinout for Ultrasonics:
+# Sensor | PW pin | RX pin
+# Left   |   D1   |   D2
+# Center |   D3   |   D4
+# Right  |   D5   |   D6
+sonics = [
+    (Pin(rx, Pin.IN), Pin(tx, Pin.OUT))
+    for rx, tx in [
+        (5, 4),
+        (0, 2),
+        # (14, 12),
+    ]
+]
+LED = Pin(16, Pin.OUT)
+
 
 def get_readings():
-    if uart.any():
-        return uart.read()
-    return [1,2,3]
+    results = []
+    for i, (rx, tx) in enumerate(sonics):
+        tx.on()
+        utime.sleep_us(30)
+        tx.off()
+
+        dt = machine.time_pulse_us(rx, 1)
+
+        results.append(dt)
+
+        utime.sleep_us(50000-dt)
+
+    return results
 
 
 PERIOD = 500  # ms
 
 while True:
     last_time = utime.ticks_ms()
+    LED.off()  # Turn on the LED
     values = get_readings()
     try:
         response = urequests.get(
@@ -38,4 +64,5 @@ while True:
     except OSError as e:
         print("Error", e)
 
+    LED.on()  # Turn off the LED
     utime.sleep_ms(max(last_time + PERIOD - utime.ticks_ms(), 0))
