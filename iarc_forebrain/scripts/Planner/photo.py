80dbf7259ecc0e8826be38367c5a1031579dedfc
Eric Miller <eric@legoaces.org>
2019-07-27 23:10:44 -0400

    Updated analyze mode to move the gimbal down.

--- iarc_forebrain/scripts/Planner/photo.py
+++ iarc_forebrain/scripts/Planner/photo.py
@@ -7,21 +7,40 @@ from iarc_arbiter.drone import Drone
 
 
 class Photo(Mode):
+	DEFAULT_PITCH_ANGLE = 30 # degrees
+	PHOTO_PITCH_ANGLE = 60 # degrees
+	WAIT_TIME = rospy.Duration.from_sec(1.0)
 	def __init__(self, drone):
 		self.drone = drone
 		rospy.Subscriber(drone.namespace+"image_raw", Image, self.image_raw_callback)
 		self.qrPub = rospy.Publisher("/qr_image", ImageBin, queue_size=10)
 		self.image = None
+		self.start_time = rospy.Time.now()
+		self.bin_number = -1
+		self.has_triggered = False
+
 	def image_raw_callback(self,data):
 		self.image = data
 
 	def enable(self, binNumber=0):
 		# makes a imagebin message and sends it to the qr node
 		self.active=True
-		imageBinMsg = ImageBin()
-		imageBinMsg.image = self.image
-		imageBinMsg.bin = int(binNumber)
-		if(self.image == None):
-			rospy.logwarn("No images are being published.")
-			return
-		self.qrPub.publish(imageBinMsg)
+		self.start_time=rospy.Time.now()
+		self.bin_number = binNumber
+		self.has_triggered = False
+		self.drone.move_camera(self.PITCH_ANGLE, 0)
+
+	def update(self, look_direction=0, obstacles=[]):
+		self.drone.hover()
+		if (not self.has_triggered and (rospy.Time.now() - self.start_time)>self.WAIT_TIME):
+			if(self.image == None):
+				rospy.logwarn("Image publishing delayed.")
+				return
+			imageBinMsg = ImageBin()
+			imageBinMsg.image = self.image
+			imageBinMsg.bin = int(self.bin_number)
+			self.qrPub.publish(imageBinMsg)
+			self.has_triggered = True
+			rospy.logwarn("Image captured from drone ")
+			self.drone.move_camera(self.DEFAULT_PITCH_ANGLE, 0)
+
